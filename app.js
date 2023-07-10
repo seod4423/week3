1e7ab476f28cd173f55787c5b4539b5c520d0512
seod4423 <seod4423@gmail.com>
2023-07-10 11:40:11 +0900

    5강 예제

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-console.log('Hello World')
+// console.log('Hello World')
+
+
+// //value
+// console.log(10)
+// console.log('Seod')
+
+// //variable
+// let firstName = 'Thomas'
+// firstName = 'Alex'
+// console.log(firstName)
+
+// //camelCase
+// let myAge = 10;
+// let MyAge = 10;
+// // let @myAge = 10;
+// // let 1myAge = 10;
+
+// //snake_case
+
+// //Data type
+
+// // 1. Number
+// console.log(10);
+// let name = 12;
+// console.log(name)
+// console.log(typeof name)
+
+// // 2. String
+// console.log('Seod')
+// let name = 'John'
+// console.log(name)
+// console.log(typeof name)
+// //template literal (ES6)
+// let myName = 'Kim'
+// let templateLiteral = `hello ${myName}`
+// console.log(templateLiteral)
+
+// // 3. Boolean
+// console.log(true)
+// let isFun = true;
+// console.log(isFun)
+// console.log(typeof isFun)
+
+// // 4. Undefined
+// let apple;
+// console.log(apple)
+// console.log(typeof apple)
+
+// // 5. Null
+// let time = null
+// console.log(time)
+// console.log(typeof time)
+
+// // 6. Symbol (ES6 = ES2015)
+// let symbol = Symbol()
+// console.log(symbol)
+// console.log('symbol')
+// console.log(typeof symbol)
+
+// // 7. BigInt (ES2020)
+// let theBiggestInt = 9002093487234998998798n;
+// console.log(theBiggestInt)
+// console.log(123123)
+// console.log(typeof 123123)
+// console.log(typeof theBiggestInt)
